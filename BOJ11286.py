b81b2d2a16a0152b895159cfbc22cfdf88f0b4a9
이성호 <1dolcong@naver.com>
2023-07-19 14:17:46 +0900

    heapq까지

---
+++ BOJ11286.py
@@ -0,0 +1,14 @@
+import heapq, sys
+num = int(input())
+pq = []
+inputList = []
+for i in range(num):
+    temp = int(sys.stdin.readline().rstrip())
+    inputList.append(temp)
+    if not temp == 0:
+        heapq.heappush(pq, (abs(temp),temp))
+    else:
+        if len(pq) > 0:
+            print(heapq.heappop(pq)[1])
+        else:
+            print("0")
